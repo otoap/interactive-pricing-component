2ff9906c123f580cd864a18077adb74d88d4646e
otoap <Otaraptsiauri@gmail.com>
2024-05-27 19:40:15 +0400

    fix calculate function

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let slider = document.getElementById("slider");
 let k = document.getElementsByTagName("h2")[0];
 let price = document.getElementsByClassName("price")[0];
 let monthYear = document.getElementsByClassName("monthYear")[0];
-console.log(monthYear)
 
+console.log(window.getComputedStyle(slider).background);
 //switcher
 switcher.addEventListener("click", ()=> {
     if(window.getComputedStyle(dot).justifySelf == "start") {
@@ -14,13 +14,11 @@ switcher.addEventListener("click", ()=> {
     }else {
         dot.style.justifySelf = "start";
     }
-    console.log(window.getComputedStyle(dot).justifySelf)
     calculate();
 })
 
 //slider
 slider.addEventListener("input", ()=> {
-    console.log(slider.value);
     calculate();
 })
 
@@ -29,28 +27,23 @@ let pageView = 0;
 let newK;
 let newPrice;
 function calculate() {
-    if(window.getComputedStyle(dot).justifySelf == "start"){
-        //pageview
-        pageView = Number(slider.value)*2;
-        newK = pageView + "K PAGEVIEWS";
-        k.innerHTML = newK;
+    //pageview
+    pageView = Number(slider.value)*2;
+    newK = pageView + "K PAGEVIEWS";
+    k.innerHTML = newK;
 
+    //background
+    slider.style.background = `rgba(0, 0, 0, 0) linear-gradient(to right, rgb(164, 243, 235) ${slider.value}%, rgb(207, 216, 239) ${slider.value}%) repeat scroll 0% 0% / auto padding-box border-box`;
+    if(window.getComputedStyle(dot).justifySelf == "start"){
         //price
         newPrice = (Number(slider.value)*0.32).toFixed(2);
         price.innerHTML = `$${newPrice}`;
         monthYear.innerHTML = "/month";
-    }else {
-        //pageview
-        pageView = Number(slider.value)*2;
-        newK = pageView + "K PAGEVIEWS";
-        k.innerHTML = newK;
 
+    }else {
         //price
         newPrice = (((Number(slider.value)*0.32*12)/100)*75).toFixed(2);
         price.innerHTML = `$${newPrice}`;
         monthYear.innerHTML = "/year";
     }
-    
-
-
 }
